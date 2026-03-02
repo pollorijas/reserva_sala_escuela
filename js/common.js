e1454af7cc2181da036c3a1888d3b9d4d26dc192
pollorijas <pollo.rijas@gmail.com>
2026-03-02 15:51:28 -0300

    -Cambio en Supabase

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 // Configuración de Supabase - REEMPLAZA CON TUS CREDENCIALES
-import { createClient } from 'npm:@supabase/supabase-js@2'
+import { createClient } from '@supabase/supabase-js@2'
 
 const supabaseUrl = 'https://iuspypmzrwzlqolbkjhl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1c3B5cG16cnd6bHFvbGJramhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1OTA5NDIsImV4cCI6MjA3NjE2Njk0Mn0.da86H7bm7lZ5T66qaNyjl1iflQ1xN-iy_5wanhdLzHE';

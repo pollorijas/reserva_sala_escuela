e20b1d61e72af55e10083431ee1764c44e7698e9
pollorijas <pollo.rijas@gmail.com>
2026-03-04 08:47:48 -0300

    supabase
    intento 1002

--- js/common.js
+++ js/common.js
@@ -9,7 +9,7 @@
 //import { createClient } from "@supabase/supabase-js";
 const { createClient } = require("@supabase/supabase-js");
 
-const supabase = createClient(
+const supabaseDB = createClient(
   process.env.SUPABASE_DATABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
